e096ab4b1c72fd12ecbe6869800702df9eed13d2
alfitodwiputra <91455397+alfitodwiputra@users.noreply.github.com>
2024-06-27 16:34:15 +0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -18,7 +18,7 @@ def play_webcam(conf, model):
     # Memutar webcam dan mendeteksi objek
     if st.sidebar.button('Detect Objects'):
         try:
-            vid_cap = cv2.VideoCapture(CAP_V4L2)
+            vid_cap = cv2.VideoCapture(source_webcam)
             st_frame = st.empty()
             while vid_cap.isOpened():
                 success, image = vid_cap.read()
